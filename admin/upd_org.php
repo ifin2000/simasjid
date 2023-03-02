86547aa18f27b8671f670a48565a94da4d294e54
Muchammad Syamsul Arifin <ifin2000@gmail.com>
2023-03-02 17:40:07 +0700

    Update upd_org.php

--- admin/upd_org.php
+++ admin/upd_org.php
@@ -7,7 +7,8 @@ $koorg = $_POST['koorg'];
 $nama = $_POST['nama'];
 $alamat = $_POST['alamat'];
 $telp = $_POST['telp'];
-$kota = $_POST['kota'];
+$kota = explode(' # ',$_POST['kota']);
+//$kota = $_POST['kota'];
 $bank = $_POST['bank'];
 $norek = $_POST['norek'];
 $anrek = $_POST['anrek'];
@@ -15,7 +16,8 @@ if ($nama==''){
     ("Location: ../dashboard.php?pages=organisasi&hasil=pesangagal2");
 } else {
     include ('../incl/koneksi.php');
-    $resukt = mysqli_query($db_link,"update organisasi set nama='$nama',kode='$koorg',alamat='$alamat',kota='$kota',telp='$telp',bank='$bank',norek='$norek',anrek='$anrek' where id='$kode'");
+    $resukt = mysqli_query($db_link,"update organisasi set id='$kota[0]',nama='$nama',kode='$koorg',alamat='$alamat',kota='$kota[1]',telp='$telp',bank='$bank',norek='$norek',anrek='$anrek' where id='$kode'");
+    //$resukt = mysqli_query($db_link,"update organisasi set nama='$nama',kode='$koorg',alamat='$alamat',kota='$kota',telp='$telp',bank='$bank',norek='$norek',anrek='$anrek' where id='$kode'");
    if ($resukt) {
         header("Location: ../dashboard.php?pages=organisasi&hasil=pesansukses2");
     } else {
